d7a83fd9da371fee99774f8121c4696bb51a2808
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-03-05 00:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+let submit  = document.querySelector("#submit");
+let username = document.querySelector("#username");
+let password = document.querySelector("#password");
+let checkbox = document.querySelector("#checkbox");
+let existing  = document.querySelector("#existing");
+
+submit.addEventListener('click',function(e){
+	e.preventDefault();
+	alert("Logged in as" ${username});
+})
