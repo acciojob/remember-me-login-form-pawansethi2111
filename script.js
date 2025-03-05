a6eda274ff0717e86e34e58a41940ccf308392db
PAWAN SETHI <66323571+pawansethi2111@users.noreply.github.com>
2025-03-05 23:40:25 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,29 @@
+// //your JS code here. If required.
+// let submit  = document.querySelector("#submit");
+// let username = document.querySelector("#username");
+// let password = document.querySelector("#password");
+// let checkbox = document.querySelector("#checkbox");
+// let existing  = document.querySelector("#existing");
+
+// existing.style.display = "none";
+
+
+// submit.addEventListener("click",function(e){
+// 	e.preventDefault();
+// 	localStorage.setItem("username",username);
+// 	localStorage.setItem("password",password);
+// 	alert("Logged in as" ${username});
+// 	submit.style.display = "none";
+// 	existing.style.display = "block";
+	
+
+// if(localStorage.getItem("username")==null){
+	
+// }
+// })
+
+
+
 //your JS code here. If required.
 let submit  = document.querySelector("#submit");
 let username = document.querySelector("#username");
@@ -5,7 +31,20 @@ let password = document.querySelector("#password");
 let checkbox = document.querySelector("#checkbox");
 let existing  = document.querySelector("#existing");
 
-submit.addEventListener('click',function(e){
+existing.style.display = "none";
+
+
+submit.addEventListener("click",function(e){
 	e.preventDefault();
+	localStorage.setItem("username",username);
+	localStorage.setItem("password",password);
 	alert("Logged in as" ${username});
+	submit.style.display = "none";
+	existing.style.display = "block";
+	
+
+if(localStorage.getItem("username")==null){
+	
+}
 })
+
